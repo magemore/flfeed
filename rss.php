91d666efcc53cd06f2cf410499ad1ab99bc68332
Alexandr Martynov <alex@magemore.com>
2019-01-24 08:59:52 +0200

    fix

--- rss.php
+++ rss.php
@@ -5,10 +5,28 @@ function getFeedData($name) {
     @system('QT_QPA_PLATFORM=offscreen phantomjs '.$name.'.js');
     return json_decode(file_get_contents('/tmp/'.$name.'.json'), true);
 }
+function validateItem($d) {
+    $s = strtolower($d['title'].' '.$d['html']);
+    if (strpos($s, 'designer') !== FALSE) return false;
+    if (strpos($s, 'angular') !== FALSE) return false;
+    if (strpos($s, 'react') !== FALSE) return false;
+    if (strpos($s, 'india') !== FALSE) return false;
+    if (strpos($s, 'c#') !== FALSE) return false;
+    return true;
+}
+function removeDuplicates($ab) {
+    $a = [];
+    foreach ($ab as $d) {
+        if (!validateItem($d)) continue;
+        $a[$d['link']] = $d;
+    }
+    return $a;
+}
 $a = [];
-foreach (['freelancer_tag_yii','freelancer_search_yii'] as $name) {
+foreach (['freelancer_tag_yii','freelancer_search_yii', 'freelancer_search_psd_html'] as $name) {
     $a = array_merge($a, getFeedData($name));
 }
+$a = removeDuplicates($a);
 $date = (new DateTime())->format('Y-m-d\TH:i:sP');
 ?><?xml version = "1.0" encoding = "UTF-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
