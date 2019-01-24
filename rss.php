4314dc1882e50496181931dc2052b9d11a14edaf
Alexandr Martynov <alex@magemore.com>
2019-01-24 08:18:59 +0200

    fix

---
+++ rss.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-type: application/atom+xml');
+@unlink('/tmp/freelancer.json');
+@system('QT_QPA_PLATFORM=offscreen phantomjs freelancer.js');
+$a = json_decode(file_get_contents('/tmp/freelancer.json'), true);
+$d = new DateTime();
+$date = $d->format('Y-m-d\TH:i:sP');
+?><?xml version = "1.0" encoding = "UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <id>http://magemore.com/rss.php</id>
+    <title>rss</title>
+    <updated><?php echo $date; ?></updated>
+    <link rel="self" href="http://magemore.com/test.atom" hreflang="en-us"/>
+    <?php foreach ($a as $d): ?>
+    <entry>
+        <id><?=$d['link']?></id>
+        <title type="html"><?=$d['title']?></title>
+        <updated><?php echo $date; ?></updated>
+        <link rel="alternate" href="<?=$d['link']?>"/>
+        <summary type="html"><![CDATA[<?=$d['html']?>]]></summary>
+        <content type="html"><![CDATA[<?=$d['html']?>]]></content>
+    </entry>
+    <?php endforeach; ?>
+</feed>
