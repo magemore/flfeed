033b701a52e9cf9d662554a1d9d542245b59c067
Alexandr Martynov <alex@magemore.com>
2019-01-24 08:31:31 +0200

    fix

--- rss.php
+++ rss.php
@@ -8,13 +8,13 @@ $date = (new DateTime())->format('Y-m-d\TH:i:sP');
 <feed xmlns="http://www.w3.org/2005/Atom">
     <id>http://magemore.com/rss.php</id>
     <title>rss</title>
-    <updated><?php echo $date; ?></updated>
+    <updated><?=$date?></updated>
     <link rel="self" href="http://magemore.com/test.atom" hreflang="en-us"/>
     <?php foreach ($a as $d): ?>
     <entry>
         <id><?=$d['link']?></id>
         <title type="html"><?=$d['title']?></title>
-        <updated><?php echo $date; ?></updated>
+        <updated><?=$date?></updated>
         <link rel="alternate" href="<?=$d['link']?>"/>
         <summary type="html"><![CDATA[<?=$d['html']?>]]></summary>
         <content type="html"><![CDATA[<?=$d['html']?>]]></content>
