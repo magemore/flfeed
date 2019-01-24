f9258bfb24ff6c8828771048b2d5f5eae2296d21
Alexandr Martynov <alex@magemore.com>
2019-01-24 13:16:09 +0200

    fix

--- application/controllers/Feed.php
+++ application/controllers/Feed.php
@@ -9,9 +9,11 @@ class Feed extends CI_Controller {
 
     public function index()
     {
+      //  @system('wget -qO- http://magemore.com/cron/ &> /dev/null &');
+
         header('Content-type: application/atom+xml');
         $this->load->database();
-        $a = $this->db->order_by('id', 'DESC')->limit(10)->get('entry')->result_array();
+        $a = $this->db->order_by('id', 'DESC')->limit(30)->get('entry')->result_array();
         foreach ($a as $i => $d) {
             $d['date'] = (new DateTime($d['created_at']))->format('Y-m-d\TH:i:sP');
             $d['summary'] = $this->htmlToText($d['html']);
